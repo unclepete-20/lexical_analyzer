a242eed8141ea343ccf829d0a07f9565a28d4b7f
Pedro Arriola <pedroparty310@gmail.com>
2023-02-24 21:04:51 -0600

    Se inicia clase Postfix para conversion de regex

---
+++ Postfix.py
@@ -0,0 +1,34 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Postfix.py
+@Date    :   2023/02/24
+@Author  :   Pedro Arriola (20188)
+@Version :   1.0
+@Desc    :   Clase para convertir un regex de formato infix a postfix.
+'''
+
+class Postfix(object):
+    
+    def __init__(self, regex):
+        self.regex = self.concatenation(regex)
+        self.postfix = self.infix_to_postfix()
+        self.stack = []
+        self.operators = ["*", "+", "|", "?", "."]
+        self.precedence = {
+            "|": 1,
+            "*": 2,
+            "+": 2,
+            "?": 2,
+            ".": 3
+        }
+    
+    
+    
+    # Para realizar dicha operacion, se utilizara el Shunting Yard Algorithm
+    def infix_to_postfix(self):
+        pass
+    
+    
+    # Permite agregar el operador de concatenacion antes de convertir el regex a postfix
+    def concatenation(self, infix_regex):
+        pass
