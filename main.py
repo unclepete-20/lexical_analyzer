531a87d95d5d419170350917d16a556736db6a78
Pedro Arriola <pedroparty310@gmail.com>
2023-03-18 21:32:32 -0600

    Se implementa verificacion de errores

--- main.py
+++ main.py
@@ -9,8 +9,14 @@
 
 from Postfix import Postfix
 from Thompson import Thompson
+from Errors import Errors
 
 r = 'a(a|b)*ab'
+
+# Se verifican errores, si los hay
+check_regex = Errors(r).valid
+print(check_regex)
+
 
 expression = Postfix(r).postfixExpression
 afn = Thompson(expression)
